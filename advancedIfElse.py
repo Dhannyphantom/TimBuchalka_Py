a003e71012b90e0404e50608f3360aa7fb4e7493
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-05-19 14:37:09 +0100

    Advanced if else

---
+++ advancedIfElse.py
@@ -0,0 +1,13 @@
+age = int(input("How old are you: "))
+
+if 16 <= age <= 65:
+    print("Have a good day")
+else:
+    print("You're not in range")
+
+bird = "Norwegian Blue"
+letter = input("Type in a character: ")
+if letter not in bird:
+    print("Have a good day")
+else:
+    print("You're not in range")
