a16d65fb74a9ef035e6ccc1328983de89087f11d
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-05-19 17:40:25 +0100

    Guessing game using while loops challenge

---
+++ challengeWhileLoop.py
@@ -0,0 +1,25 @@
+import random
+
+print("Welcome to the Guessing game by python")
+print("Select a difficulty of 1 - 100")
+maxNumber = int(input())
+
+answer = random.randint(1, maxNumber)
+print("Please guess a number between 1 and {}".format(maxNumber))
+
+result = int(input())
+counter = 1
+
+while result != answer:
+    counter += 1
+    if result > answer:
+        print("Guess Lower")
+        result = int(input())
+    else:
+        print("Guess Higher")
+        result = int(input())
+
+if counter == 1:
+    print("You got it the first try")
+else:
+    print("You guessed correctly in {} tries".format(counter))
